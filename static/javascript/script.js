b03f0b1fdd5f9f005d05f5e6aefe2b6ec5d7fc0e
Filming <79931565+filming@users.noreply.github.com>
2024-08-20 14:25:12 -0400

    feat: Make an operation handler

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -16,3 +16,21 @@ function divide(num1, num2){
     }
     return num1 / num2;
 }
+
+function operate(operator, firstNumber, secondNumber){
+    switch (operator){
+        case "add":
+            return add(firstNumber, secondNumber);
+
+        case "subtract":
+            return subtract(firstNumber, secondNumber);
+
+        case "multiply":
+            return multiply(firstNumber, secondNumber);
+
+        case "divide":
+            return divide(firstNumber, secondNumber);
+    }
+}
+
+let firstNumber, secondNumber, operator;
